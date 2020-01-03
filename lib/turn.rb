a3445f55a9c46e94e5d2aba89e6d8f4997bbeb8d
Qoosim <plenty4luv@yahoo.com>
2020-01-03 19:31:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,10 +7,8 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8)
-    if !position_taken?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
     true
-    end
   else
     false
   end
